91d409e8db027d009b13bedf38dd8c44735f2d1a
Tousar Mohammed <Tousar@Tousars-MBP.lan>
2018-06-20 17:06:38 -0500

    first commit

---
+++ CMakeFiles/cv_hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_hello_world.dir/cv_hello_world.cpp.o"
+  "cv_hello_world.pdb"
+  "cv_hello_world"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
